2ea7bec1f2c8069c2d3080a2d68d2b4c6c4e52f7
Anton B <antonbsa@gmail.com>
2022-03-03 02:16:21 -0300

    submit form and save transaction

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Server } from 'miragejs'
+import { Model, Server } from 'miragejs'
 import { App } from './App';
 
 new Server({
+  models: {
+    transaction: Model,
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food'
-        }
-      ]
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create('transaction', data);
     })
   }
 })
