d90b66b793277495b74f7625d574054944091cab
Anton B <antonbsa@gmail.com>
2022-03-03 00:53:02 -0300

    add transaction modal UI

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,10 @@
 import Modal from "react-modal";
+import { Container, TransactionTypeContainer } from "./styles";
+
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,8 +16,51 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-      <h2>Cadastrar Transação</h2>
+      <button
+        className="react-modal-close"
+        onClick={onRequestClose}
+        type="button"
+      >
+        <img src={closeImg} alt="Fechar" />
+      </button>
+
+      <Container>
+        <h2>Cadastrar Transação</h2>
+
+        <input
+          placeholder="Título"
+        />
+        <input
+          type="number"
+          placeholder="Valor"
+        />
+
+        <TransactionTypeContainer>
+          <button
+            type="button"
+          >
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+
+          <button
+            type="button"
+          >
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
+        <input
+          placeholder="Categoria"
+        />
+        <button type="submit">
+          Cadastrar
+        </button>
+      </Container>
     </Modal>
   )
 }
